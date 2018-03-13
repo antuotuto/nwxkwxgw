9fb11b02acd37a586a0f220f904c2f4da0b0378e
wanganan <675988829@qq.com>
2018-03-13 09:35:22 +0800

    1.0.0

---
+++ my-project/src/pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+      <p>你好{{userInfo.nickName}}</p>
+
+      <span>是的</span>
+  </div>
+</template>
+
+<script>
+import card from "@/components/card";
+
+export default {
+  data() {
+    return {
+      motto: "Hello World",
+      userInfo: {}
+    };
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap() {
+      const url = "../logs/logs";
+      wx.navigateTo({ url });
+    },
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo;
+            }
+          });
+        }
+      });
+    },
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
+    }
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
